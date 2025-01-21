c95222f25316eb75748e9befae089032d8309726
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2025-01-21 19:27:04 +0700

    add struct for equipment put api

--- src/data/request/equipment_request.go
+++ src/data/request/equipment_request.go
@@ -48,3 +48,75 @@ func ValidateMuscleGroup(muscleGroups []string) bool {
 	}
 	return true
 }
+
+type EquipmentPutRequest struct {
+	AdditionalField *AdditionalFieldPut `json:"additional_field,omitempty"`
+	Band            *string             `json:"band,omitempty"`
+	Color           *string             `json:"color,omitempty"`
+	Images          *Images             `json:"images,omitempty"`
+	Material        *string             `json:"material,omitempty"`
+	Model           *string             `json:"model,omitempty"`
+	MuscleGroupUsed []string            `json:"muscle_group_used,omitempty"`
+	Name            *string             `json:"name,omitempty"`
+	Option          *OptionPut          `json:"option,omitempty"`
+	SpecialFeature  *string             `json:"special_feature,omitempty"`
+}
+
+type AdditionalFieldPut struct {
+	Created []AdditionalFieldCreated `json:"created,omitempty"`
+	Deleted []string                 `json:"deleted,omitempty"`
+	Updated []AdditionalFieldUpdated `json:"updated,omitempty"`
+}
+
+type AdditionalFieldCreated struct {
+	Key   *string `json:"key,omitempty"`
+	Value *string `json:"value,omitempty"`
+}
+
+type AdditionalFieldUpdated struct {
+	ID    *string `json:"id,omitempty"`
+	Key   *string `json:"key,omitempty"`
+	Value *string `json:"value,omitempty"`
+}
+
+type Images struct {
+	DeletedID []DeletedID `json:"deleted_id"`
+	UploadID  []UploadID  `json:"upload_id"`
+}
+
+type DeletedID struct {
+	ID        *string `json:"id,omitempty"`
+	IsPrimary *bool   `json:"is_primary,omitempty"`
+	URL       *string `json:"url,omitempty"`
+}
+
+type UploadID struct {
+	ID        *string `json:"id,omitempty"`
+	IsPrimary *bool   `json:"is_primary,omitempty"`
+}
+
+type OptionPut struct {
+	Added   []Added         `json:"added"`
+	Created []OptionCreated `json:"created"`
+	Deleted []string        `json:"deleted"`
+	Updated []OptionUpdated `json:"updated"`
+}
+
+type Added struct {
+	Available *int64   `json:"available,omitempty"`
+	Price     *float64 `json:"price,omitempty"`
+	Weight    *float64 `json:"weight,omitempty"`
+}
+
+type OptionCreated struct {
+	Available *int64   `json:"available,omitempty"`
+	Price     *float64 `json:"price,omitempty"`
+	Weight    *float64 `json:"weight,omitempty"`
+}
+
+type OptionUpdated struct {
+	Available *int64   `json:"available,omitempty"`
+	ID        *string  `json:"id,omitempty"`
+	Price     *float64 `json:"price,omitempty"`
+	Weight    *float64 `json:"weight,omitempty"`
+}
