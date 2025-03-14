fcc7a65576f7c17bc7b8f880ef4290fc89d4334b
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2025-03-14 11:02:26 +0700

    remove unique on role

--- src/model/user.go
+++ src/model/user.go
@@ -12,7 +12,7 @@ type User struct {
 	LastName    string    `gorm:"type:varchar(100);not null" json:"last_name"`
 	Email       string    `gorm:"type:varchar(255);unique;not null" json:"email"`
 	Password    string    `gorm:"type:varchar(255);not null" json:"-"`
-	Role        enum.Role `gorm:"gorm:type:role_type;unique;not null;default:'USER'" json:"role"`
+	Role        enum.Role `gorm:"gorm:type:role_type;not null;default:'USER'" json:"role"`
 	Address     string    `gorm:"type:text" json:"address"`
 	PhoneNumber string    `gorm:"type:varchar(20)" json:"phone_number"`
 	Orders      []Order   `gorm:"foreignKey:UserID" json:"orders"`
