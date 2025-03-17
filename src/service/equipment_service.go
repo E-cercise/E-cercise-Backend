56b5aa461d80aa4c5273ef33747bbd09656cb3a2
ThanadolU <dolthanadolu@gmail.com>
2025-03-17 23:55:04 +0700

    changed the way to check range of budget from repository level to service level

--- src/service/equipment_service.go
+++ src/service/equipment_service.go
@@ -40,7 +40,7 @@ func (s *equipmentService) GetEquipmentData(q request.EquipmentListRequest, pagi
 	if q.MuscleGroup != "" {
 		muscleGroup = strings.Split(q.MuscleGroup, ",")
 	}
-	equipments, err := s.equipmentRepo.FindEquipmentList(q.Q, muscleGroup, paginator, q.Category, int64(q.Min), int64(q.Max))
+	equipments, err := s.equipmentRepo.FindEquipmentList(q.Q, muscleGroup, paginator, q.Category)
 
 	if err != nil {
 		logger.Log.WithError(err).Error("error during find all equipments")
@@ -61,14 +61,16 @@ func (s *equipmentService) GetEquipmentData(q request.EquipmentListRequest, pagi
 
 		price := findEquipmentMinimumPrice(equipment)
 
-		eq := response.Equipment{
-			ID:              equipment.ID,
-			Name:            equipment.Name,
-			Price:           price,
-			ImagePath:       imagePath,
-			MuscleGroupUsed: helper.GetMuscleGroupIDFromEquipment(equipment),
+		if (q.Min == 0 || price >= q.Min) && (q.Max == 0 || price <= q.Max) {
+			eq := response.Equipment{
+				ID:              equipment.ID,
+				Name:            equipment.Name,
+				Price:           price,
+				ImagePath:       imagePath,
+				MuscleGroupUsed: helper.GetMuscleGroupIDFromEquipment(equipment),
+			}
+			resp.Equipments = append(resp.Equipments, eq)
 		}
-		resp.Equipments = append(resp.Equipments, eq)
 	}
 	return &resp, nil
 }
