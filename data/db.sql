d6f5828594f22f6f6850f70128f472922e96e62b
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2025-04-08 17:46:14 +0700

    modify sql

--- data/db.sql
+++ data/db.sql
@@ -29072,7 +29072,7 @@ ALTER TABLE ONLY public.equipment_muscle_groups
 --
 
 ALTER TABLE ONLY public.images
-    ADD CONSTRAINT fk_equipment_options_images FOREIGN KEY (equipment_option_id) REFERENCES public.equipment_options(id);
+    ADD CONSTRAINT fk_equipment_options_images FOREIGN KEY (equipment_option_id) REFERENCES public.equipment_options(id) on delete cascade;
 
 
 --
